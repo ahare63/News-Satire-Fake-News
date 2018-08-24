32a13d0d63054deec3471e205d2ea22b9d70b5b6
Adam Hare <adamth63@gmail.com>
2018-08-23 21:28:18 -0400

    [WIP] Comments, readme

--- Python/NNTrainNobel.py
+++ Python/NNTrainNobel.py
@@ -24,18 +24,18 @@ from keras.callbacks import EarlyStopping
 from keras.regularizers import l2
 import tensorflow as tf
 
-# fixes out of memory errors on Nobel
+# Fixes out of memory errors on Nobel.
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 K.set_session(tf.Session(config=config))
 
 
-# function to append title to start of body so that it is included in the analysis
+# Function to append title to start of body so that it is included in the analysis.
 def get_raw_text(row):
     return row.Title + ". " + row.Body
 
 
-# calculate precision -> taken from old Keras source code
+# Calculate precision -> taken from old Keras source code
 def get_prec(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
@@ -43,7 +43,7 @@ def get_prec(y_true, y_pred):
     return precision
 
 
-# calculate recall -> taken from old Keras source code
+# Calculate recall -> taken from old Keras source code
 def get_rec(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
@@ -51,42 +51,42 @@ def get_rec(y_true, y_pred):
     return recall
 
 
-# get F Score
+# Get F Score.
 def get_f(y_true, y_predict):
     pre = get_prec(y_true, y_predict)
     rec = get_rec(y_true, y_predict)
     return (2*pre*rec)/(pre + rec)
 
-# read data from csv files
+# Read data from csv files.
 train = pd.read_csv('../Data/train.csv')
 train = train.drop(['Unnamed: 0'], axis=1).dropna(how='any', subset={'Body'}).sample(frac=1)
 test = pd.read_csv('../Data/test.csv')
 test = test.drop(['Unnamed: 0'], axis=1).dropna(how='any', subset={'Body'}).sample(frac=1)
 
-# remove unnecessary fields, append title to start of body
+# Remove unnecessary fields, append title to start of body.
 train = train[['Body', 'Title', 'Source', 'isSatire']]
 train['rawText'] = train.apply(lambda x: get_raw_text(x), axis=1)
 test = test[['Body', 'Title', 'Source', 'isSatire']]
 test['rawText'] = test.apply(lambda x: get_raw_text(x), axis=1)
 print('Ready to Tokenize')
 
-# fit tokenizer on text
-# about 1/3 has greater than 100 uses
-# 501,279 unique words in entire corpus, 222,169 only used once 401,906 used 10 or fewer
+# Fit the tokenizer on text.
+# About 1/3 of words have more than 100 uses.
+# There are 501,279 unique words in entire corpus; 222,169 are used only once and 401,906 used 10 or fewer times.
 maxWords = 20000
 tokenizer = Tokenizer(num_words=maxWords)
 tokenizer.fit_on_texts(train.rawText)
 asSequence = tokenizer.texts_to_sequences(train.rawText)
 
-# maxlen = len(max(asSequence, key=len))  # to preserve length of longest article
-maxlen = 4000  # reduced in practice to save computing time
-xTrain = np.array(pad_sequences(asSequence, maxlen=maxlen))  # pad articles for use in CNN
+# maxlen = len(max(asSequence, key=len))  # This would preserve length of longest article. Not practical due to heavy tails.
+maxlen = 4000  # Reduced in practice to save computing time.
+xTrain = np.array(pad_sequences(asSequence, maxlen=maxlen))  # Pad articles for use in CNN.
 testAsSequence = tokenizer.texts_to_sequences(test.rawText)
 xTest = np.array(pad_sequences(testAsSequence, maxlen=maxlen))
-xTest[xTest > maxWords] = 0  # reserved for unknown string in preprocessing -> remove words from test not in train
+xTest[xTest > maxWords] = 0  # Reserved for unknown string in preprocessing -> remove words from test not in train
 
-# build the CLSTM model with Keras
-# the parameters given here are an example and should be fit according to the data
+# Build the CLSTM model with Keras.
+# The parameters given here are an example. In general, they should be fit according to the data.
 model_CLSTM = Sequential()
 model_CLSTM.add(Embedding(maxWords + 1, 100, input_length=maxlen))
 model_CLSTM.add(Dropout(0.3))
@@ -94,16 +94,16 @@ model_CLSTM.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
 model_CLSTM.add(LSTM(units=64))
 model_CLSTM.add(Dense(1, activation='sigmoid'))
 
-# compile the model
+# Compile the model.
 model_CLSTM.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['accuracy', get_prec, get_rec, get_f])
 callbacks = [EarlyStopping(monitor='loss')]
 
-# fit the CLSTM
+# Fit the CLSTM.
 model_CLSTM.fit(xTrain, y=np.array(train.isSatire), callbacks=callbacks, batch_size=512, epochs=10, shuffle=True)
                 # class_weight={0: 1, 1: 10})  # add class weight if desired
     
-# evaluate the data and print the results
+# Evaluate the data and print the results.
 a = model_CLSTM.evaluate(xTest, np.array(test.isSatire))
 print(a)
-# print formatted for LaTeX table
+# Print formatted for LaTeX table.
 print(" %.4f & %.4f & %.4f & %.4f \\\\" % (a[1], a[2], a[3], a[4]))
--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # Classifying News, Satire, and "Fake News": An SVM and Deep Learning Approach
 Senior Thesis completed as part of coursework for Princeton University.
+This thesis was awarded the S.S. Wilkes Memorial Prize by the Princeton Operations Research and Financial Engineering department for the best thesis on "statistics and its applications to solving societal problems."
 
-This repository contains the final report, some of the code, and data used in my senior thesis titled *Classifying News, Satire, and "Fake News": An SVM and Deep Learning Approach*.
+This repository contains the final report, some of the code, and data used in my senior thesis titled *Classifying News, Satire, and "Fake News": An SVM and Deep Learning Approach*. The code is formatted to run on Princeton's Nobel and Adroit computing clusters. See the pdf for the full thesis report
 
-The code is requires several libraries, chiefly pandas, scikit-learn, and keras. Changing some file locations may be necessary to run directly as these programs were set up to run on Princeton's Nobel and Adroit clusters.
+Before running, unzip both files in the "Data" folder. They are quite sizeable and cause rather long runtimes even on high-performance clusters.
 
 Raw data files and code for processing them is omitted. Included instead is allData.zip, which contains allSatire.csv and allSerious.csv. These files contain all satirical and all serious news articles used as data for this project. Also included are train.csv and test.csv which were used for training and testing satire against serious news in the project. Note that these are two divisions of the same data.
+
+Some code has been refactored from what was used in the actual testing to be more readable and modular. Due to the time crunch many values where hard-coded and hand-edited which would have been better taken as input from the command line. This code is mostly included for reference and to give an idea of the procedures used in this thesis.
+
+# Dependencies
+This thesis makes use of several standard Python libraries, specifically:
+* `pandas`
+* `numpy`
+* `nltk`
+* `keras`
+* `tensorflow`
+* `sklearn`
+* `scipy`
+* `text.stat` 
+* `re`
+* `csv`
