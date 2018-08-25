4609237b7b71fae2a51354f8f1f341406da2b3e5
Adam Hare <adamth63@gmail.com>
2018-08-24 22:55:09 -0400

    [WIP] Added formatter capability to API

--- Python/parserLib.py
+++ Python/parserLib.py
@@ -8,10 +8,6 @@ This file contains a number of functions used to parse data from the csv files. 
 time-intensive depending on the size of the data set.
 """
 
-import pandas as pd
-import numpy as np
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
 from textstat.textstat import textstat
 import re
 import csv
@@ -34,116 +30,29 @@ def get_profanity_count(data, dict_file):
         return data['Body'].apply(lambda x: profanity_count(x, profane_list))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Do one-hot encoding on the date. Assigns a one to the column corresponding to the year provided and a one to all
+# others. If the date is NaN or out of range, it leaves all columns as zero. Optionally takes a range of supported
+# years, by default 2010-2017 inclusive.
+def get_encoded_date(data, date_range):
+    for date in date_range:
+        data[str(date)] = data["Date"].apply(lambda x: int(x == date))
+    return data
 
 
 # Function to get the average number of syllables per word.
-# Here, row refers to the article being considered. isTitle is a boolean, which when True 
-# calculates the average syllable count for the title. When isTitle is False, this function 
+# Here, row refers to the article being considered. is_title is a boolean, which when `True`
+# calculates the average syllable count for the title. When isTitle is False, this function
 # returns the average syllable count for the body of text.
-def getAvgSyl(row, isTitle):
-    if isTitle == True:
+def get_avg_syl_count(row, is_title):
+    if is_title:
         syls = textstat.syllable_count(row.Title)
         return syls/row.titleWordCount
     syls = textstat.syllable_count(row.Body)
     return syls/row.wordCount
 
 
-# Count the number of links/Twitter pictures.
-def linkCount(text):
+# Count the number of links and Twitter pictures in the provided text.
+def get_link_count(text):
     num = len(re.findall('http(s)?://', text))
     num += len(re.findall('pic\.twitter\.com/[A-Za-z0-9]* — .* \(@[A-Za-z0-0]*\)', text))
     return num
-
-# # Read the data from the csv files.
-# allSatire = pd.read_csv('../Data/allSatire.csv')
-# allSatire = allSatire.drop(['Unnamed: 0'], axis=1)  # drops extra unnecessary column added by pandas
-# allSerious = pd.read_csv('../Data/allSerious.csv')
-# allSerious = allSerious.drop(['Unnamed: 0'], axis=1).dropna(how='any')
-#
-# # Add satire label. A value of one indicates that the article is satirical.
-# allSatire['isSatire'] = np.repeat(1, len(allSatire.Body))
-# allSerious['isSatire'] = np.repeat(0, len(allSerious.Body))
-#
-# # Perform one-hot encoding on the date field. A value of 1 indicates that the article was written in a given year.
-# # Note that if no date is provided, these will all be 0.
-# allSatire["2010"] = allSatire["Date"].apply(lambda x: int(x == 2010))
-# allSerious["2010"] = allSerious["Date"].apply(lambda x: int(x == 2010))
-# allSatire["2011"] = allSatire["Date"].apply(lambda x: int(x == 2011))
-# allSerious["2011"] = allSerious["Date"].apply(lambda x: int(x == 2011))
-# allSatire["2012"] = allSatire["Date"].apply(lambda x: int(x == 2012))
-# allSerious["2012"] = allSerious["Date"].apply(lambda x: int(x == 2012))
-# allSatire["2013"] = allSatire["Date"].apply(lambda x: int(x == 2013))
-# allSerious["2013"] = allSerious["Date"].apply(lambda x: int(x == 2013))
-# allSatire["2014"] = allSatire["Date"].apply(lambda x: int(x == 2014))
-# allSerious["2014"] = allSerious["Date"].apply(lambda x: int(x == 2014))
-# allSatire["2015"] = allSatire["Date"].apply(lambda x: int(x == 2015))
-# allSerious["2015"] = allSerious["Date"].apply(lambda x: int(x == 2015))
-# allSatire["2016"] = allSatire["Date"].apply(lambda x: int(x == 2016))
-# allSerious["2016"] = allSerious["Date"].apply(lambda x: int(x == 2016))
-# allSatire["2017"] = allSatire["Date"].apply(lambda x: int(x == 2017))
-# allSerious["2017"] = allSerious["Date"].apply(lambda x: int(x == 2017))
-#
-# # Get word count on both the body and title.
-# allSatire['wordCount'] = allSatire['Body'].apply(lambda x: len(word_tokenize(x)))
-# allSatire['titleWordCount'] = allSatire['Title'].apply(lambda x: len(word_tokenize(x)))
-# allSerious['wordCount'] = allSerious['Body'].apply(lambda x: len(word_tokenize(x)))
-# allSerious['titleWordCount'] = allSerious['Title'].apply(lambda x: len(word_tokenize(x)))
-#
-# # Count the number of profane words.
-# allSatire['profanityCount'] = allSatire['Body'].apply(lambda x: profanityCount(x))
-# allSerious['profanityCount'] = allSerious['Body'].apply(lambda x: profanityCount(x))
-#
-# # Get the average number of syllables per word for both the body and title.
-# allSatire['avgSyl'] = allSatire.apply(lambda x: getAvgSyl(x, False), axis=1)
-# allSatire['titleAvgSyl'] = allSatire.apply(lambda x: getAvgSyl(x, True), axis=1)
-# allSerious['avgSyl'] = allSerious.apply(lambda x: getAvgSyl(x, False), axis=1)
-# allSerious['titleAvgSyl'] = allSerious.apply(lambda x: getAvgSyl(x, True), axis=1)
-#
-# # Get reading ease scores for both the body and title.
-# allSatire['FR'] = allSatire['Body'].apply(lambda x: textstat.flesch_reading_ease(x))
-# allSatire['GF'] = allSatire['Body'].apply(lambda x: textstat.gunning_fog(x))
-# allSatire['ARI'] = allSatire['Body'].apply(lambda x: textstat.automated_readability_index(x))
-#
-# allSatire['titleFR'] = allSatire['Title'].apply(lambda x: textstat.flesch_reading_ease(x))
-# allSatire['titleGF'] = allSatire['Title'].apply(lambda x: textstat.gunning_fog(x))
-# allSatire['titleARI'] = allSatire['Title'].apply(lambda x: textstat.automated_readability_index(x))
-#
-# allSerious['FR'] = allSerious['Body'].apply(lambda x: textstat.flesch_reading_ease(x))
-# allSerious['GF'] = allSerious['Body'].apply(lambda x: textstat.gunning_fog(x))
-# allSerious['ARI'] = allSerious['Body'].apply(lambda x: textstat.automated_readability_index(x))
-#
-# allSerious['titleFR'] = allSerious['Title'].apply(lambda x: textstat.flesch_reading_ease(x))
-# allSerious['titleGF'] = allSerious['Title'].apply(lambda x: textstat.gunning_fog(x))
-# allSerious['titleARI'] = allSerious['Title'].apply(lambda x: textstat.automated_readability_index(x))
-#
-# # Get number of sentences.
-# allSatire['senCount'] = allSatire['Body'].apply(lambda x: len(sent_tokenize(x)))
-# allSerious['senCount'] = allSerious['Body'].apply(lambda x: len(sent_tokenize(x)))
-#
-# # Get number of links and Twitter characters.
-# allSatire['linkCount'] = allSatire['Body'].apply(lambda x: linkCount(x))
-# allSatire['twitChar'] = allSatire['Body'].apply(lambda x: len(re.findall('[@#]', x)))
-#
-# allSerious['linkCount'] = allSerious['Body'].apply(lambda x: linkCount(x))
-# allSerious['twitChar'] = allSerious['Body'].apply(lambda x: len(re.findall('[@#]', x)))
-#
-# # Write the data back to the original csv files.
-# allSatire.to_csv('./Data/combinedSets/allSatire.csv')
-# allSerious.to_csv('./Data/combinedSets/allSerious.csv')
-#
-# print('Done!')
--- Python/python-api.py
+++ Python/python-api.py
@@ -8,8 +8,13 @@ This file contains a few higher level functions to handle data parsing, SVM test
 CLSTM testing.
 """
 
+from nltk.tokenize import word_tokenize, sent_tokenize
+import numpy as np
 import pandas as pd
-from parserLib import get_profanity_count
+import re
+from textstat.textstat import textstat
+
+from parserLib import get_profanity_count, get_encoded_date, get_avg_syl_count, get_link_count
 
 
 """
@@ -18,22 +23,85 @@ formatted as in one of the provided csv files, although not all fields are neede
 `Date`, and `Title` columns are required although they can be empty. All parsing is off by default.
 
     Parameters:
-        filename - the name of the csv file from which to read the initial data
-        profane_dict_file - the name of the csv file from which to read the dictionary of profane words
-        count_profane - if True, count the number of profane words and place in 'profanityCount' column
+        # Note: all boolean parameters are `False` by default. This indicates that no calculation will be made. If this
+        # function is passed only the filename parameter, it will build and return a `pandas` `DataFrame` read from the
+        # csv specified by filename. Additionally, running each of these will overwrite whatever is in the column they
+        # write to but not change any other columns.
+    
+        filename - The name of the csv file from which to read the initial data.
         
         write_to_file - Whether or not to write to the input file. If `False`, no data is written. If `True`, data is 
-                        written to the file specified by the filename parameter.
+                        written to the file specified by the filename parameter. `False` by default.
+                        
         target_file - If specified, a target file to write to. Otherwise, no file is written. Both write_to_file and
                       target_file can write in a given call.
+                      
+        count_profane - If `True`, count the number of profane words and place in 'profanityCount' column.
+        
+        profane_dict_file - The name of the csv file from which to read the dictionary of profane words. By default,
+                            'profaneWords.csv'. Setting this has no effect if count_profane=False. This csv should 
+                            contain only regular expressions of what will be identified as profane words. See included 
+                            file `profaneWords.csv` for an example.
+                            
+        is_satire - If specified, indicates whether or not an article is satire. Default is -1, which skips adding a
+                    label. If 0, every article in the file is given a label of 0 for non-satirical. If 1, every article
+                    is given a label of 1 for satirical. Throws an error if given something other than -1, 0, or 1.
+                    Writes to 'isSatire' column.
+                    
+        encode_date - A boolean value indicating whether or not to do one-hot encoding on the date field. `True` does
+                      the encoding. Writes to columns with the year as the name.
+                      
+        date_range - A range of dates to consider. Only valid if encode_date=True. Default is 2010-2017 inclusive.
+        
+        title_word_count - A boolean indicating whether or not to generate a word count for the title. Writes to 
+                           'titleWordCount' column if `True`.
+                           
+        word_count - A boolean indicating whether or not to generate a word count for the body. Writes to 'wordCount' 
+                     column if `True`.
+                     
+        title_syl_count - A boolean indicating whether or not to generate a title average syllable count. Writes to 
+                         'titleAvgSyl' column if `True`. Only valid if titleWordCount column is populated.
+                         
+        body_syl_count - A boolean indicating whether or not to generate a body average syllable count.  Writes to 
+                         'avgSyl' column if `True`. Only valid if wordCount column is populated.
+                         
+        sentence_count - A boolean indicating whether or not to generate a count of the number of sentences in the body.
+                         Writes to 'senCount' column if `True`.
+                         
+        link_count - A boolean indicating whether or not to generate a count of the number of links found in the body
+                     of the articles.  Writes to 'linkCount' column if `True`.
+                     
+        twitter_count - A boolean indicating whether or not to generate a count of the number of Twitter characters
+                        found in the body. Writes to 'twitChar' column if `True`.
+                        
+        title_fr_score - A boolean indicating whether or not to run Flesch Reading Ease scoring algorithm on the title. 
+                         Writes to 'titleFR' column if `True`.
+                         
+        fr_score - A boolean indicating whether or not to run Flesch Reading Ease scoring algorithm on the body. 
+                   Writes to 'FR' column if `True`.
+                        
+        title_gf_score - A boolean indicating whether or not to run the Gunning Fog scoring algorithm on the title. 
+                         Writes to 'titleGF' column if `True`.
+                         
+        gf_score - A boolean indicating whether or not to run the Gunning Fog scoring algorithm on the body. 
+                   Writes to 'GF' column if `True`.
+                   
+        title_ari_score - A boolean indicating whether or not to run the Automated Readability Index algorithm on the 
+                          title. Writes to 'titleARI' column if `True`.
+                         
+        ari_score - A boolean indicating whether or not to run the Automated Readability Index algorithm on the body. 
+                    Writes to 'ARI' column if `True`.
                         
     Returns:
         This function returns a `pandas` `DataFrame` object with the desired parsed fields added.
 """
 
 
-def parse_data(filename, profane_dict_file='profaneWords.csv', count_profane=False, write_to_file=False,
-               target_file=''):
+def parse_data(filename, write_to_file=False, target_file='', count_profane=False, profane_dict_file='profaneWords.csv',
+               is_satire=-1, encode_date=False, date_range=range(2010, 2018), title_word_count=False, word_count=False,
+               title_syl_count=False, body_syl_count=False, sentence_count=False, link_count=False, twitter_count=False,
+               title_fr_score=False, fr_score=False, title_gf_score=False, gf_score=False, title_ari_score=False,
+               ari_score=False):
 
     # Read the data from the csv file.
     data = pd.read_csv(filename, index_col=0)
@@ -41,6 +109,64 @@ def parse_data(filename, profane_dict_file='profaneWords.csv', count_profane=Fal
     # Count the number of profane words.
     if count_profane:
         data['profanityCount'] = get_profanity_count(data, profane_dict_file)
+
+    # Add satirical label.
+    if is_satire == 0 or is_satire == 1:
+        data['isSatire'] = np.repeat(is_satire, len(data.Body))
+    elif is_satire != -1:
+        raise ValueError('Invalid value for is_satire: should be -1, 0, or 1')
+
+    # Do one-hot encoding on the date.
+    if encode_date:
+        get_encoded_date(data, date_range)
+    elif date_range != range(2010, 2018):
+        raise ValueError('Date range cannot be set if encode_date=False')
+
+    # Get word counts.
+    if title_word_count:
+        data['titleWordCount'] = data['Title'].apply(lambda x: len(word_tokenize(x)))
+    if word_count:
+        data['wordCount'] = data['Body'].apply(lambda x: len(word_tokenize(x)))
+
+    # Do average syllable counts.
+    if title_syl_count:
+        if 'titleWordCount' not in data.columns:
+            raise IndexError('Must generate title word count before calculating title average syllable count ')
+        data['titleAvgSyl'] = data.apply(lambda x: get_avg_syl_count(x, False), axis=1)
+    if body_syl_count:
+        if 'wordCount' not in data.columns:
+            raise IndexError('Must generate word count before calculating average syllable count ')
+        data['avgSyl'] = data.apply(lambda x: get_avg_syl_count(x, True), axis=1)
+
+    # Get sentence counts.
+    if sentence_count:
+        data['senCount'] = data['Body'].apply(lambda x: len(sent_tokenize(x)))
+
+    # Get the number of links in the body.
+    if link_count:
+        data['linkCount'] = data['Body'].apply(lambda x: get_link_count(x))
+
+    # Get the number of Twitter characters in the body
+    if twitter_count:
+        data['twitChar'] = data['Body'].apply(lambda x: len(re.findall('[@#]', x)))
+
+    # Generate Flesch Reading Ease score.
+    if title_fr_score:
+        data['titleFR'] = data['Title'].apply(lambda x: textstat.flesch_reading_ease(x))
+    if fr_score:
+        data['FR'] = data['Body'].apply(lambda x: textstat.flesch_reading_ease(x))
+
+    # Generate Gunning Fog score.
+    if title_gf_score:
+        data['titleGF'] = data['Title'].apply(lambda x: textstat.gunning_fog(x))
+    if gf_score:
+        data['GF'] = data['Body'].apply(lambda x: textstat.gunning_fog(x))
+
+    # Generate Automated Readability Index score.
+    if title_ari_score:
+        data['titleARI'] = data['Title'].apply(lambda x: textstat.automated_readability_index(x))
+    if ari_score:
+        data['ARI'] = data['Body'].apply(lambda x: textstat.automated_readability_index(x))
 
     # Write to file filename.
     if write_to_file:
@@ -52,6 +178,3 @@ def parse_data(filename, profane_dict_file='profaneWords.csv', count_profane=Fal
 
     # Return `DataFrame` object
     return data
-
-
-print(parse_data('../Data/smallTrainSample.csv'))
