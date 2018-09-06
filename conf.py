5b6b9bc03f47e26c107830977379803dc28fa041
Adam Hare <adamth63@gmail.com>
2018-09-05 21:39:22 -0400

    [WIP] Doc update

--- conf.py
+++ conf.py
@@ -106,7 +106,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Senior_Thesisdoc'
+htmlhelp_basename = 'Senior Thesisdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -133,7 +133,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Senior_Thesis.tex', 'Senior\\_Thesis Documentation',
+    (master_doc, 'Senior Thesis.tex', 'Senior Thesis Documentation',
      'Adam Hare', 'manual'),
 ]
 
@@ -143,7 +143,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'senior_thesis', 'Senior_Thesis Documentation',
+    (master_doc, 'senior thesis', 'Senior Thesis Documentation',
      [author], 1)
 ]
 
@@ -154,8 +154,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Senior_Thesis', 'Senior_Thesis Documentation',
-     author, 'Senior_Thesis', 'One line description of project.',
+    (master_doc, 'Senior Thesis', 'Senior Thesis Documentation',
+     author, 'SeniorvThesis', 'One line description of project.',
      'Miscellaneous'),
 ]
 
